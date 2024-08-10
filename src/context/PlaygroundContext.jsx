6cfef84f0d439109134dcb17ffdd06dc4725f318
adgupta-1 <21bec029@iiitdmj.ac.in>
2024-08-10 20:23:24 +0530

    'third'

--- src/context/PlaygroundContext.jsx
+++ src/context/PlaygroundContext.jsx
@@ -4,24 +4,6 @@ import { v4 as uuid } from 'uuid';
 export const PlaygroundContext = createContext();
 
 export const languageMap = {
-    "cpp": {
-        id: 54,
-        defaultCode: 
-        "#include <iostream>\n"
-        + "using namespace std;\n\n"
-        + "int main() {\n"
-        + '\tcout << "Hello World!";\n'
-        + "\treturn 0;\n"
-        + "}",
-    },
-    "java": {
-        id: 62,
-        defaultCode: `public class Main {
-            public static void main(String[] args) {
-                System.out.println("Hello World!");
-            }
-    }`,
-    },
     "python": {
         id: 71,
         defaultCode: `print("Hello World!")`,
@@ -40,8 +22,8 @@ const PlaygroundProvider = ({ children }) => {
             playgrounds: {
                 [uuid()]: {
                     title: "Stack Implementation",
-                    language: "cpp",
-                    code: languageMap["cpp"].defaultCode,
+                    language: "python",
+                    code: languageMap["python"].defaultCode,
                 },
                 [uuid()]: {
                     name: "Array",
